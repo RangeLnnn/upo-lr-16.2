b5b3418bac1b8eddc85fcfcc7b0ae9e22f0f3ee6
Четырбок Максим <ChetyMak_88@students.mgke.loc>
2026-04-15 12:23:30 +0300

    about all but need to add 3 func for cart(add,upload,delete)

--- project/shop/urls.py
+++ project/shop/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns=[
@@ -8,7 +9,12 @@ urlpatterns=[
     path('',views.mainpage),
     path('catalog/',views.catalog),
     path('catalog/<int:pk>/',views.produkt_detail),
-    path('cart',views.cart),
+    path('cart/',views.cart),
+    path('registrate/',views.registrate),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    #path('cart/add/<int:product_id>',views.add_product),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     
+
 
 ]
--- project/shop/views.py
+++ project/shop/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
+from django.contrib.auth.decorators import login_required
 def aboutauthor(request):
     return render(request,'aboutauthor.html')
 def aboutshop(request):
@@ -13,9 +15,28 @@ def catalog(request):
 def produkt_detail(request,pk):
     product = Product.objects.get(pk=pk)
     return render(request,'productdetail.html',{'product':product})
+@login_required
 def cart(request):
     cart = Cart.objects.filter(user_id=request.user.id).first()
     if cart:
         items = CartsElement.objects.filter(cart_id=cart.id)
+    else:
+        items = []
     return render(request,'cart.html',{'items':items})
+def registrate(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request,'registrate.html',{'form': form})
+
+
+
+
+
+
+
 # Create your views here.
