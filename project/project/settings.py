8436707a35f0d7b7e7d6dec0fc207b71dd625bfd
Maksim <maksimchetyrbok@gmail.com>
2026-06-22 21:51:47 +0300

    .

--- project/project/settings.py
+++ project/project/settings.py
@@ -9,11 +9,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-am=w18rcpm+yt_39yix73
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config(
-    'ALLOWED_HOSTS', 
-    default='127.0.0.1,localhost', 
-    cast=lambda v: [s.strip() for s in v.split(',')]
-)
+ALLOWED_HOSTS = ['.up.railway.app', 'upo-lr-162-production.up.railway.app', 'localhost', '127.0.0.1']
 
 if not DEBUG:
     ALLOWED_HOSTS.append('.onrender.com')
