a8cc164b0402b1afb180f20e8d79e2ee034547aa
Maksim <maksimchetyrbok@gmail.com>
2026-06-22 22:08:43 +0300

    .

--- project/project/settings.py
+++ project/project/settings.py
@@ -100,7 +100,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = '/shop/catalog/'
 LOGOUT_REDIRECT_URL = '/shop/catalog/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
